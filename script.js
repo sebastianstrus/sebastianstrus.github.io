d1b1c375461fa4354051507972dbc374fa6e9e30
Sebastian Strus <sebastian.strus1@gmail.com>
2017-04-19 23:30:53 +0200

    Added scroll animation

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
+    $("html, body").animate({ scrollTop: "0px" }); // Chrome, Firefox etc.
+    //document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    //document.documentElement.scrollTop = 0; // For IE and Firefox
 }
+
+
+
